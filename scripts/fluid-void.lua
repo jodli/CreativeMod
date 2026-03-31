062b67a71dc5dce6cf36cb5f84940f5621e0a24d
jodli <jobst87@gmail.com>
2026-03-31 08:39:25 +0200

    fix: reverse iteration in fluid_void.tick() to prevent skipping

--- scripts/fluid-void.lua
+++ scripts/fluid-void.lua
@@ -6,7 +6,8 @@ end
 -- Processes the fluid_void table in storage.
 function fluid_void.tick()
   -- Loop through the table of fluid-void to nullify the fluid inside its fluid box.
-  for index, fluid_void in ipairs(storage.creative_mode.fluid_void) do
+  for index = #storage.creative_mode.fluid_void, 1, -1 do
+    local fluid_void = storage.creative_mode.fluid_void[index]
     if fluid_void.valid then
       if not fluid_void.to_be_deconstructed(fluid_void.force) then
         for i = 1, #fluid_void.fluidbox do
