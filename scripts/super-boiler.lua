386dcce1a8687414997ab81452e332abd3e26bfd
jodli <jobst87@gmail.com>
2026-03-31 08:39:35 +0200

    fix: reverse iteration in super_boiler.tick() to prevent skipping

--- scripts/super-boiler.lua
+++ scripts/super-boiler.lua
@@ -6,7 +6,8 @@ end
 -- Processes the table of super_boiler in storage.
 function super_boiler.tick()
   -- Loop through the table of super-boiler to give free energy and heat up fluid inside.
-  for index, super_boiler in ipairs(storage.creative_mode.super_boiler) do
+  for index = #storage.creative_mode.super_boiler, 1, -1 do
+    local super_boiler = storage.creative_mode.super_boiler[index]
     if super_boiler.valid then
       if not super_boiler.to_be_deconstructed(super_boiler.force) then
         super_boiler.energy = 1000000000000000
